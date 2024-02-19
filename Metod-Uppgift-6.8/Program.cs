7b9e177264789289b7d09a30199d09ed1691fd95
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-19 23:01:14 +0100

    Add project files.

---
+++ Metod-Uppgift-6.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Uppgift_6_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skriv in ett heltal för att få reda på vilka tal mindre än det angivna talet är ett primtal");
+            Console.WriteLine(MindrePrim(int.Parse(Console.ReadLine())));
+        }
+
+        static int MindrePrim(int tal)
+        {
+            int resultat = 0; 
+
+            for (int i = 2; i < tal; i++)
+            {
+                if (Prim(i)) resultat++;
+            }
+
+            return resultat;
+        }
+
+        static bool Prim(int tal)
+        {
+            for (int i = 2; i < tal - 1; i++)
+            {
+                if (tal % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
